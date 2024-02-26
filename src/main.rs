1df8baf7d6d547084d6b7f969520eee3ac5ba000
LemonNeko <chheese048@gmail.com>
2024-02-26 12:48:03 +0800

    tidy code

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,15 @@
+mod commands;
 mod config;
 mod models;
 mod schema;
 
-use chrono::Utc;
+
 use diesel::prelude::*;
 use dotenvy::dotenv;
-use models::{get_all_reminders, save_new_reminder};
-use std::str::FromStr;
-use std::sync::{Arc, Mutex};
-use teloxide::{prelude::*, utils::command::BotCommands, RequestError};
+use models::{get_all_reminders};
 
-#[derive(BotCommands, Clone)]
-#[command(
-    rename_rule = "snake_case",
-    description = "欢迎使用 ReminderBot，以下是可以使用的命令："
-)]
-enum Command {
-    #[command(description = "显示所有可用命令")]
-    Help,
-    #[command(description = "允许或禁止非管理员使用 ReminderBot")]
-    SetAllowAllMember(bool),
-    #[command(description = "修改 ReminderBot 使用的时区")]
-    SetTimezone(String),
-    #[command(description = "新建提醒事项")]
-    NewReminder(String),
-    #[command(description = "删除提醒事项")]
-    DeleteReminder(i32),
-    #[command(description = "显示所有提醒项")]
-    ListReminders,
-    #[command(description = "显示 ReminderBot 相关信息")]
-    About,
-}
+use std::sync::{Arc, Mutex};
+use teloxide::{prelude::*};
 
 #[tokio::main]
 async fn main() {
@@ -56,11 +35,11 @@ async fn main() {
 
     tokio::join!(
         create_cron_tasks(bot_token_cloned_2, db_cloned_2),
-        Command::repl(
+        commands::Command::repl(
             Bot::new(bot_token),
-            move |bot: Bot, msg: Message, cmd: Command| {
+            move |bot: Bot, msg: Message, cmd: commands::Command| {
                 log::trace!("creating command processor");
-                process_cmd(bot, msg, cmd, db.clone())
+                commands::process_cmd(bot, msg, cmd, db.clone())
             }
         )
     );
@@ -130,98 +109,4 @@ async fn create_cron_tasks(bot_token: String, db: Arc<Mutex<PgConnection>>) {
 
     scheduler.start().await.expect("failed to start cron tasks");
     tokio::time::sleep(std::time::Duration::from_secs(100)).await;
-}
-
-async fn process_cmd(
-    bot: Bot,
-    msg: Message,
-    cmd: Command,
-    db: Arc<Mutex<PgConnection>>,
-) -> Result<(), RequestError> {
-    match cmd {
-        Command::Help => {
-            bot.send_message(msg.chat.id, Command::descriptions().to_string())
-                .await?
-        }
-        Command::NewReminder(arg) => process_new_reminder(bot, msg, arg, db).await?,
-        Command::DeleteReminder(_id) => {
-            bot.send_message(msg.chat.id, "command not implemented")
-                .await?
-        }
-        Command::ListReminders => {
-            bot.send_message(msg.chat.id, "command not implemented")
-                .await?
-        }
-        Command::SetAllowAllMember(_allow) => {
-            bot.send_message(msg.chat.id, "command not implemented")
-                .await?
-        }
-        Command::About => {
-            bot.send_message(msg.chat.id, "command not implemented")
-                .await?
-        }
-        Command::SetTimezone(_timezone) => {
-            bot.send_message(msg.chat.id, "command not implemented")
-                .await?
-        }
-    };
-
-    Ok(())
-}
-
-async fn process_new_reminder(
-    bot: Bot,
-    msg: Message,
-    arg: String,
-    db: Arc<Mutex<PgConnection>>,
-) -> Result<Message, RequestError> {
-    let blank_index = arg.find(' ').unwrap_or(0);
-    let (c, exp) = arg.split_at(blank_index);
-
-    // TODO: check if administrator
-    log::info!(
-        "received new reminder request from {}, cron expression '{exp}'",
-        msg.chat.id
-    );
-
-    log::trace!("parsing cron expression");
-
-    let parse_result = match cron::Schedule::from_str(exp) {
-        Ok(result) => result,
-        Err(why) => {
-            log::trace!("cron expression not valid, {:?}", why);
-
-            return bot
-                .send_message(msg.chat.id, format!("{exp} 不是一个有效的 cron 表达式"))
-                .await;
-        }
-    };
-
-    log::trace!("inserting data to database");
-
-    let result = save_new_reminder(
-        msg.chat.id.0,
-        msg.chat.id.0,
-        String::from(c),
-        String::from(exp),
-        db,
-    );
-    if result.is_err() {
-        log::warn!(
-            "error when insert reminder data: {:?}",
-            result.err().unwrap()
-        );
-        return bot
-            .send_message(msg.chat.id, "出现了一些问题，请稍后再试")
-            .await;
-    }
-
-    log::trace!("get next 5th runs time");
-    let mut next_5_runs = "好的，你的提醒项已经保存，将来 5 次提醒时间如下：\n".to_string();
-    for date_time in parse_result.upcoming(Utc).take(5) {
-        next_5_runs.push_str(date_time.to_string().as_str());
-        next_5_runs.push('\n');
-    }
-
-    bot.send_message(msg.chat.id, next_5_runs).await
 }
